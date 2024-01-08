95e005c4619778e0ea86634628a6c831df054631
Aamir Pervez <aamirpervez27@gmail.com>
2024-01-08 20:42:02 +0500

    Update index.js

--- REPL Example/index.js
+++ REPL Example/index.js
@@ -2,4 +2,48 @@
 //2: use Variables 
 //3: Multiline code
 //4: use(_) to get the last result
+//5: we can use editor mode
+
+---- (Give browser example with express and variables)
+
+//REPL
+//1: js Expression (Test on Node CMD - REPL, Its combination of operand and operators) 
+5 + 5
+10
+
+
+//2: use Variables 
+var a='hello';
+a
+
+hello
+
+
+//3:Multiline code
+
+var x=0;
+x
+
+do{
+x++;
+console.log(`Value of x is ${x}`);
+}while(x <5);
+
+
+//4: use(_) to get the last result
+
 //5: we an use editor mode
+const callname = (myname) => {console.log(`My Name is ${myname}`);}
+callname('Aamir')
+
+
+
+//1: js Expression 
+//2: use Variables 
+//3:Multiline code
+//4: use(_) to get the last result
+//5: we an use editor mode
+
+
+
+------------------------------------------------------------------------
