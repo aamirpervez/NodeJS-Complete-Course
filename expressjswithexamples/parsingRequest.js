98fa67ca93c5d51800eb2eae6b1380130597dd27
Aamir Pervez <aamirpervez27@gmail.com>
2024-04-09 17:45:06 +0500

    updated views

--- expressjswithexamples/parsingRequest.js
+++ expressjswithexamples/parsingRequest.js
@@ -16,11 +16,11 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname,'public')));
 
 app.use(homeRouter);
-app.use('/admin',adminRouter);
+app.use('/admin',adminRouter.routes);
 
 app.use((req,res,next) =>{
-    //res.status(404).send('<h1>Page was not found!</h1>')
-    res.sendFile(path.join(__dirname,'./','views','404.html'))
+    res.status(404)
+    .sendFile(path.join(__dirname,'./','views','404.html'))
 })
 
 app.listen(port, () => {
